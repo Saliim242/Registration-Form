4ce61b47ddb0862002e373953a46e9914516ff4f
Babaa242 <baba242a@gmail.com>
2022-12-11 07:29:18 +0300

    Search Teacher

--- lib/screens/teachers.dart
+++ lib/screens/teachers.dart
@@ -15,6 +15,7 @@ class Teachers extends StatefulWidget {
 
 class _TeachersState extends State<Teachers> {
   List<Map<String, dynamic>> _teachers = [];
+  List<Map<String, dynamic>> _found = [];
 
   bool _isLoading = true;
   // This function is used to fetch all data from the database
@@ -23,14 +24,42 @@ class _TeachersState extends State<Teachers> {
     setState(() {
       _teachers = data;
       _isLoading = false;
-      print("..number of teachers ${_teachers.first}");
+      _found = _teachers;
+      print("..number of Founds ${_found.length}");
     });
   }
 
   @override
   void initState() {
+    _teachers = _found;
     super.initState();
-    _getAllData(); // Loading the diary when the app starts
+
+    _getAllData();
+    // Loading the diary when the app starts
+
+    print("Found is ===");
+    print(_teachers.length);
+  }
+
+  // This function is called whenever the text field changes
+  void _runFilter(String enteredKeyword) {
+    List<Map<String, dynamic>> results = [];
+    if (enteredKeyword.isEmpty) {
+      // if the search field is empty or only contains white-space, we'll display all users
+      _getAllData();
+      results = _teachers;
+    } else {
+      results = _teachers
+          .where((user) =>
+              user["name"].toLowerCase().contains(enteredKeyword.toLowerCase()))
+          .toList();
+      // we use the toLowerCase() method to make it case-insensitive
+    }
+
+    setState(() {
+      _found = results;
+      print("Yaa Huuuu");
+    });
   }
 
   @override
@@ -38,66 +67,126 @@ class _TeachersState extends State<Teachers> {
     return Scaffold(
       backgroundColor: Color(0xff263159).withOpacity(0.7),
       body: SafeArea(
-        child: _isLoading
-            ? const Center(
-                child: CircularProgressIndicator(),
-              )
-            : ListView.builder(
-                itemCount: _teachers.length,
-                itemBuilder: (context, index) => SizedBox(
-                  height: 120,
-                  child: Card(
-                    color: Color(0xff495579),
-                    margin: const EdgeInsets.all(15),
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10),
-                      child: Row(
-                        children: [
-                          Text(
-                            _teachers[index]['id'].toString(),
-                            style: TextStyle(
-                              fontSize: 20,
-                              color: Color(0xffD6E4E5).withOpacity(0.8),
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          SizedBox(width: 10),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 10),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              //mainAxisAlignment: MainAxisAlignment.spaceBetween,,
-                              children: [
-                                Text(
-                                  _teachers[index]['name'],
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                    color: Color(0xffD6E4E5).withOpacity(0.8),
-                                  ),
-                                ),
-                                Text(
-                                  _teachers[index]['phone'].toString(),
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                    color: Color(0xffD6E4E5).withOpacity(0.8),
-                                  ),
-                                ),
-                                Text(
-                                  _teachers[index]['job'],
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                    color: Color(0xffD6E4E5).withOpacity(0.8),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
+        child: Column(
+          children: [
+            SizedBox(height: 30),
+            Container(
+              //margin: EdgeInsets.symmetric(horizontal: 10),
+              margin: const EdgeInsets.all(15),
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: Color(0xff495579).withOpacity(0.4),
+                borderRadius: BorderRadius.circular(14),
+              ),
+              child: TextField(
+                onChanged: ((value) {
+                  setState(() {
+                    _runFilter(value);
+                  });
+                }),
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Color(0xffD6E4E5).withOpacity(0.8),
+                ),
+                decoration: InputDecoration(
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+                  border: InputBorder.none,
+                  hintText: "Search Teacher by Name",
+                  hintStyle: TextStyle(
+                    fontSize: 16,
+                    color: Color(0xffD6E4E5).withOpacity(0.8),
+                  ),
+                  suffixIcon: Icon(
+                    Icons.search,
+                    size: 30,
+                    color: Color(0xffD6E4E5).withOpacity(0.8),
                   ),
                 ),
               ),
+            ),
+            _isLoading
+                ? const Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : Expanded(
+                    child: _found.isNotEmpty
+                        ? ListView.builder(
+                            itemCount: _found.length,
+                            itemBuilder: (context, index) => SizedBox(
+                              key: ValueKey(_found[index]["id"]),
+                              height: 120,
+                              child: Card(
+                                color: Color(0xff495579),
+                                margin: const EdgeInsets.all(15),
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 10),
+                                  child: Row(
+                                    children: [
+                                      Text(
+                                        _found[index]['id'].toString(),
+                                        style: TextStyle(
+                                          fontSize: 20,
+                                          color: Color(0xffD6E4E5)
+                                              .withOpacity(0.8),
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      SizedBox(width: 10),
+                                      Padding(
+                                        padding: const EdgeInsets.symmetric(
+                                            vertical: 10),
+                                        child: Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          //mainAxisAlignment: MainAxisAlignment.spaceBetween,,
+                                          children: [
+                                            Text(
+                                              _found[index]['name'],
+                                              style: TextStyle(
+                                                fontSize: 20,
+                                                color: Color(0xffD6E4E5)
+                                                    .withOpacity(0.8),
+                                              ),
+                                            ),
+                                            Text(
+                                              _found[index]['phone'].toString(),
+                                              style: TextStyle(
+                                                fontSize: 20,
+                                                color: Color(0xffD6E4E5)
+                                                    .withOpacity(0.8),
+                                              ),
+                                            ),
+                                            Text(
+                                              _found[index]['job'],
+                                              style: TextStyle(
+                                                fontSize: 20,
+                                                color: Color(0xffD6E4E5)
+                                                    .withOpacity(0.8),
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          )
+                        : Text(
+                            textAlign: TextAlign.center,
+                            "Sorry No Results Found",
+                            style: TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xffD6E4E5).withOpacity(0.8),
+                            ),
+                          ),
+                  ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Color(0xff495579),
